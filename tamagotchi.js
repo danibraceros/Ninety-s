a8f7194d674e571684f9622b4d881f052ba1ef99
rakimzubair <46544797+rakimzubair@users.noreply.github.com>
2019-02-09 18:58:27 -0800

    Update tamagotchi.js

--- tamagotchi.js
+++ tamagotchi.js
@@ -1,4 +1,4 @@
-/* configurations*/
+
 var configs = { //list diff moods, age, hunger, and health states (array of configurations)
   mood: ["happy", "neutral", "sad"],
   age: ["baby", "child", "teen", "adult", "senior"],
@@ -6,7 +6,7 @@ var configs = { //list diff moods, age, hunger, and health states (array of conf
   hungry: ["famished", "normal", "hungry", "starving"],
   love: ["hate", "normal", "friendly", "infatuated"]
 }
-/* gotchi object */
+
 function Gotchi(name, age, hunger, love, mood) {
   this.name = name;
   this.age = age;
@@ -34,11 +34,14 @@ Gotchi.prototype.lifecycle = function () {
 
   function changeMood() {
     objName.mood = configs.mood[objName.indexMood];
-    objName.indexMood++;
 
-    if ((objName.hp != -1) && (objName.indexMood < 4)) {
-      console.log(objName.mood);
-      setTimeout(changeMood, 1000);
+    if (objName.indexMood < 2) {
+      objName.indexMood++;
+    }
+
+    if (objName.hp != -1) {
+      console.log("Your TAMAGOTCHI is " + objName.mood + ".");
+      setTimeout(changeMood,1000);
     }
   }
 
@@ -51,6 +54,7 @@ Gotchi.prototype.lifecycle = function () {
 
     if (objName.hp < 0) {
       objName.hp = -1;
+      console.log("Your TAMAGOTCHI is dead. :(");
     }
 
     if (objName.hp <= 2) {
@@ -72,7 +76,6 @@ Gotchi.prototype.lifecycle = function () {
   }
 
   function checkLp() {
-
     if (objName.lp > 10) {
       objName.lp = 10;
     }
@@ -97,18 +100,18 @@ Gotchi.prototype.lifecycle = function () {
       objName.love = configs.love[3];
     }
 
-    if (objName.hp < 4 && objName.lp < 3) {
+    if(objName.hp < 4 && objName.lp < 3){
       objName.health = configs.health[1];
     }
   }
 
-  function checkAge() {
+  function checkAge(){
 
     if (objName.age <= 3) {
       objName.phase = configs.age[0];
     }
 
-    else if (objName.age <= 4 && objName.age > 12) {
+    else if (objName.age <= 4 && objName.age > 12 ) {
       objName.phase = configs.age[1];
     }
 
@@ -125,61 +128,57 @@ Gotchi.prototype.lifecycle = function () {
     }
   }
 
-  function getDanindu() {
-    console.log(objName.name + ' is a goon');
-    //setTimeout(getDanindu, 2000);
-  }
-
-  function getHungry() {
-    if (objName.hp > -1) {
+  function getHungry(){
+    if (objName.hp > -1){
       objName.hp--;
-      console.log("hp " + objName.hp)
+      console.log("Your TOMOGATCHI has " + objName.hp + " hunger points.")
       checkHp();
-      setTimeout(getHungry, 1000);
+      setTimeout(getHungry,1000);
     }
   }
 
-  function getOld() {
-    if (objName.hp != -1) {
+  function getOld(){
+      if(objName.hp != -1){
       objName.age++;
-      console.log("age " + objName.age);
+      console.log("Your TAMAGOTCHI is " + objName.age + " years old.");
+      console.log("-------------------------------------");
       checkAge();
-      setTimeout(getOld, 1000);
-    }
-  }
+      setTimeout(getOld,1000);
+      }
+  }  
 
-  function lowerLove() {
-    if ((objName.lp > 0) && (objName.hp != -1)) {
+  function lowerLove(){
+    if ((objName.lp > 0) && (objName.hp != -1)){
       objName.lp--;
-      console.log("lp " + objName.lp)
+      console.log("Your TAMAGOTCHI has " + objName.lp + " love points.")
       checkLp();
-      //console.log("lp " + objName.lp)
-      setTimeout(lowerLove, 1000);
+      setTimeout(lowerLove,1000);
     }
-  }
-
-  function feed() {
+  }    
+  
+  function feed(){
     objName.hp += 2;
 
-    if (hp >= 15) {
+    if(hp >= 15){
       objName.health = configs.health[1];
     }
 
-    if (hp >= 25) {
+    if(hp >= 25){
       objName.hp = -1;
+      console.log("You fed your TAMAGOTCHI too much!");
     }
   }
 
-  function play() {
-    if (objName.lp > 10) {
-      objName.lp += 2;
+  function play(){
+    if (objName.lp > 10){
+    objName.lp += 2;
     }
   }
 
-  /*setTimeout(getHungry, 1000);
+  setTimeout(getHungry, 1000);
   setTimeout(lowerLove, 1000);
   setTimeout(getOld, 2000);
-  setTimeout(changeMood, 1000);*/
+  setTimeout(changeMood, 1000);
 }
 
 var danindu = new Gotchi('Danindu', 2, 'full', 'normal', 'happy');
